085d9b0cfd597a8823579f04e3f8cfb45ffafff4
nsanthony <nsanthony@gmail.com>
2020-03-07 14:51:04 -0500

    added string generator. Testing to see if I can get anything working.

---
+++ Comet/StreamProcessor/src/main/java/streamprocessor/filterstream/source/GeneratedData.java
@@ -0,0 +1,39 @@
+package streamprocessor.filterstream.source;
+
+import org.apache.flink.api.dag.Transformation;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
+
+public class GeneratedData extends RichSourceFunction<String> {
+
+    private boolean running = true;
+
+    @Override
+    public void run(SourceContext<String> sourceContext) throws Exception {
+
+        String testOutputString = "testOutput";
+
+        while(running){
+            sourceContext.collect(testOutputString);
+        }
+
+    }
+
+    @Override
+    public void cancel() {
+        this.running = false;
+    }
+
+
+    /**
+     * Create a new {@link DataStream} in the given execution environment with
+     * partitioning set to forward by default.
+     *
+     * @param environment    The StreamExecutionEnvironment
+     * @param transformation
+     */
+
+
+
+}
