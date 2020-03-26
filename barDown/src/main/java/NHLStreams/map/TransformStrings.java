82ae959e9654049f3faabde11162f85db8b74674
nsanthony <nsanthony@gmail.com>
2020-03-26 14:53:07 -0400

    Added the files for BarDown

---
+++ barDown/src/main/java/NHLStreams/map/TransformStrings.java
@@ -0,0 +1,19 @@
+package NHLStreams.map;
+
+import org.apache.flink.api.common.functions.MapFunction;
+
+import java.nio.charset.Charset;
+import java.util.Random;
+
+public class TransformStrings implements MapFunction<String, String> {
+
+    @Override
+    public String map(String value) throws Exception {
+        int randomNumCharacters = new Random().nextInt(100);
+        byte[] randomBytes = new byte[randomNumCharacters];
+        new Random().nextBytes(randomBytes);
+        String randomCharacters = new String(randomBytes, Charset.forName("UTF-8"));
+        String appendedString = value + randomCharacters;
+        return appendedString;
+    }
+}
