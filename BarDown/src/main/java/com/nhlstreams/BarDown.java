305dd6ad613440bc094367fba69a0b0696d6cac8
nsanthony <nsanthony@gmail.com>
2020-03-28 14:45:41 -0400

    Added the most recent nifi template

--- BarDown/src/main/java/com/nhlstreams/BarDown.java
+++ BarDown/src/main/java/com/nhlstreams/BarDown.java
@@ -2,6 +2,7 @@ package com.nhlstreams;
 
 import com.google.gson.Gson;
 import com.nhlstreams.source.NifiBarDownDataStream;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -14,10 +15,12 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.nifi.NiFiDataPacket;
 import org.apache.flink.streaming.connectors.nifi.NiFiSource;
 
+import org.apache.flink.streaming.connectors.pulsar.PulsarSourceBuilder;
 import org.apache.nifi.remote.client.SiteToSiteClient;
 import org.apache.nifi.remote.client.SiteToSiteClientConfig;
 
 import com.nhlstreams.map.ParseNHLJson;
+import org.apache.pulsar.shade.org.codehaus.jackson.schema.JsonSchema;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -33,6 +36,14 @@ public class BarDown {
         env.getConfig().setGlobalJobParameters(params);
 
         try {
+
+            PulsarSourceBuilder<String> pulsarSourceBuilder = PulsarSourceBuilder.builder(new SimpleStringSchema());
+
+            SourceFunction<String> pulsarSource = pulsarSourceBuilder.build();
+
+            DataStream<String> pulsarStream = env.addSource(pulsarSource);
+
+
             SiteToSiteClientConfig nifiConfig = new SiteToSiteClient.Builder()
                     .url("http://192.168.1.39:8080/nifi")
                     .portIdentifier("7c6548e9-acc8-31c1-8201-5942e1cde3ac")
