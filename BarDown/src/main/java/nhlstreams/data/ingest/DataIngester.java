14936a349da9679aba9f23db5ac4d2e72586d607
nsanthony <nsanthony@gmail.com>
2021-02-18 17:48:54 -0500

    Adding ingester

---
+++ BarDown/src/main/java/nhlstreams/data/ingest/DataIngester.java
@@ -0,0 +1,16 @@
+package nhlstreams.data.ingest;
+
+import com.google.common.flogger.FluentLogger;
+
+public class DataIngester {
+	private static final FluentLogger logger = FluentLogger.forEnclosingClass();   	
+
+	
+	
+	public void parseData(String input) {
+		//this needs to do something. What input?
+		
+		
+		
+	}
+}
