b62b5bd9b5c0b5c99aa8e1d6cff3628aff24967e
Nathan Anthony <nsanthony@Nathans-MBP.fios-router.home>
2021-02-28 08:42:15 -0500

    Parsing Json and adding it to src/test/resources

--- BarDown/src/test/java/nhlstreams/data/controller/DataControllerTests.java
+++ BarDown/src/test/java/nhlstreams/data/controller/DataControllerTests.java
@@ -2,6 +2,7 @@ package nhlstreams.data.controller;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
@@ -10,16 +11,22 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.google.common.flogger.FluentLogger;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class DataControllerTests {
 	private static final FluentLogger logger = FluentLogger.forEnclosingClass();  
 	private static String baseUrl;
 	private static String eventExtension;	
+	private static String path;
 	
 	@BeforeAll
 	public static void setup() {
 		baseUrl = "http://statsapi.web.nhl.com/api/v1";
 		eventExtension = "/game/2017020608/feed/live";
+		path = "event.json";
+		
 	}
 	
 	@Test
@@ -29,7 +36,12 @@ public class DataControllerTests {
 		HttpResponse<String> event = null;
 		try {
 			event = dataCtl.getLatestEvent(eventExtension);
-			System.out.print("\nGot this response: " + event.body());
+			JsonObject jsonObject = new JsonParser().parse(event.body()).getAsJsonObject();
+			
+			Gson gson = new Gson();
+			gson.toJson(jsonObject, new FileWriter(path));
+			
+			logger.atInfo().log("\nGot this response: " + event.body());
 		} catch (URISyntaxException | IOException | InterruptedException e) {
 			logger.atSevere().withCause(e)
 				.log("Failed to get event");
