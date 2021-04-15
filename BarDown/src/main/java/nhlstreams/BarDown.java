3dbb335cd49743df3c0c8d6e867749117e70fd23
Nathan Anthony <nsanthony@Nathans-MBP.fios-router.home>
2021-04-14 21:25:28 -0400

    testing pushing and doing some formatting

--- BarDown/src/main/java/nhlstreams/BarDown.java
+++ BarDown/src/main/java/nhlstreams/BarDown.java
@@ -1,17 +1,14 @@
 package nhlstreams;
 
-import java.lang.System.Logger;
-
 import com.google.common.flogger.FluentLogger;
 
 public class BarDown {
-	private static final FluentLogger logger = FluentLogger.forEnclosingClass();   
-	
-	
+	private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
 	public static void main(String[] args) {
-		
-		//this will be the main we launch the streaming application from. 
-		
+
+		// this will be the main we launch the streaming application from.
+
 	}
-	
+
 }
